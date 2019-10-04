7cbe58a9960bba9ace4e39c950d19c1da9341289
Pazuzu (PJ) Jindrich <Pazuzujindrich@gmail.com>
2019-10-04 11:42:03 -0700

    Add files via upload

---
+++ CheatActivity.java
@@ -0,0 +1,65 @@
+package com.example.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    //Extra sent by MainActivity on whether the answer is true
+    private static final String EXTRA_ANSWER_IS_TRUE = "";
+
+    //Extra to be received by MainActivity on whether the user cheated
+    private static final String EXTRA_ANSWER_IS_SHOWN = "";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
+
+    //creating an intent and including an extra along with it
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+    public static boolean wasAnswerShown(Intent result){
+        return result.getBooleanExtra(EXTRA_ANSWER_IS_SHOWN, false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        //set the default value to false if key is not found
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        //initialize the textview
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        //initialize button
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                }else{
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+    private void setAnswerShownResult(boolean isAnswerShown){
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_IS_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
